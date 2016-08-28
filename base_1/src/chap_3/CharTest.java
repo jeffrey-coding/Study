3082dd7f5931d08aa06f12ba2b049e7e4b48e2f9
jeffrey sun <jeffrey.coding@foxmail.com>
2016-08-28 16:49:19 +0800

    add chars

---
+++ base_1/src/chap_3/CharTest.java
@@ -0,0 +1,11 @@
+package chap_3;
+
+public class CharTest {
+	public static void main(String[] args) {
+		char aChar = 'a';
+		char enterChar = '\r';
+		char ch = '\u9999';
+		System.out.println(ch);
+		
+	}
+}
